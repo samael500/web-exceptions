01c3390f7e4144b82faa60fee2f3352f4d6b0909
Maks <samael500@gmail.com>
2017-05-14 17:36:43 +0300

    :repeat: remove not used code from example app

--- example/example/settings.py
+++ example/example/settings.py
@@ -42,20 +42,18 @@ INSTALLED_APPS = [
     # they should be added here
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    # add middleware for dj exceptions
+    'web_exceptions.middleware.WebExceptionsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-MIDDLEWARE = [
-    # add middleware for dj exceptions
-    'web_exceptions.middleware.WebExceptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'example.urls'
